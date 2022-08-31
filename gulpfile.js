d55a6006fc7d0fb8bf997a016fb427ef65ee529f
wangyang <wangyang.0210@foxmail.com>
2022-09-01 01:21:53 +0800

    package: update devdependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-let gulp = require('gulp');
-let minifycss = require('gulp-minify-css');
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('minify-css', function () {
     return gulp.src('./src/style/simpleMemory.css')
-        .pipe(minifycss())
+        .pipe(cleanCSS())
         .pipe(gulp.dest('./dist'));
 });
 
