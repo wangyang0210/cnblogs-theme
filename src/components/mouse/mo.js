e71cb1cfef398da5a39995cadf3dfec26c5c0f7d
wangyang <wangyang.0210@foxmail.com>
2022-09-04 19:40:36 +0800

    dist: test for google-fonts

---
+++ src/components/mouse/mo.js
@@ -0,0 +1,11 @@
+/**
+ * UPDATES AND DOCS AT: https://github.com/wangyang0210
+ * https://www.cnblogs.com/wangyang0210/
+ * @author: BNTang, BNTang.303158131@qq.com
+ * @Date 2022-08-27 20:29
+ * ----------------------------------------------
+ * @describe: 炸开点击特效
+ */
+export default function main(_) {
+
+}
