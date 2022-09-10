70103856b248b418d810619aa80ce98347ae5936
wangyang <wangyang.0210@foxmail.com>
2022-09-10 20:18:38 +0800

    iconfont-foods

--- src/components/articleInfo/lib/iconfontMap.js
+++ src/components/articleInfo/lib/iconfontMap.js
@@ -8,24 +8,23 @@
  */
 
 
-const food = [
-                'hebaodan','bingtanghulu','kesong','qianceng','fengmi','feiyuguantou','shengjian','youtiao','yuzijiang',
-                'zhutongfan','doujiang','sanmingzhi','paofu','shanbei','dangaojuan','futejia','huangyou','xiangchang',
-                'banji','danta','qingning','lajiao','shizi','mojituo','pijiu','putaojiu','kouxiangtang','xiangcaobingqilin',
-                'jiaozi','tilamisu','huoguo','hongshu','bingkuai','mianhuatang','paobing','meishikafei','mantou','qishui',
-                'ganlan','jiroujuan','guodong','baozi','pingguo','chengzi','qingjiao','jidan','xihongshi','mangguo','baocai',
-                'niunai','mianbao','huluobu','zhangyu','pangxie','longxia','yangcong','rou','jitui','huage','xianyu','mogu',
-                'qiezi','xilanhua','ningmeng','liulian','banli','sanwenyu','tudou','xigua','nangua','huolongguo','fantuan',
-                'zhusun','shuiluobu','shanzhu','lanmei','shiliu','yezi','tiangua','mihoutao','boluo','kaixinguo','hetao',
-                'xiaweiyiguo','huasheng','bigenguo','kuihuazi','songzi','xiguazi','badanmu','yaoguo','danhuangsu','dangao',
-                'binggan','buding','tangguo','qiaokeli','hongzao','candou','putaogan','manyuemei','taozi','xiangjiao','caomei',
-                'niuyouguo','hamigua','chelizi','li','bale','kafei1','shutiao','zhenzhunaicha','xuegao','nailao','kele','tiantong',
-                'hanbao','xiezishousi','baomihua','regou','makalong','tianfuluo','juzi','baixiangguo','putao','shaomai','yumi','pipa',
-                'yangtao','youzi','lianwu','wuhuaguo','paomian','wandou','huanggua','suantou','tiantianquan','shupian','huafubing',
-                'bangbangtang','shousi','lizhi','doufu','mocha','boluomi','zhouzi','bingsha','suannai','pisa','haixing','haizhe',
-                'tongluoshao','nuomici','kuangquanshui','roujiamo','cha','zhangyuxiaowanzi','chengzhi','yuancaitou','baicai'
-            ]
+const foods = [
+    'hongzao', 'putaogan', 'kele', 'kuangquanshui', 'futejia', 'pijiu', 'putaojiu', 'baozi',
+    'mianbao', 'fantuan', 'hanbao', 'xiezishousi', 'baomihua', 'regou', 'tianfuluo', 'shaomai',
+    'shousi', 'pisa', 'nuomici', 'roujiamo', 'zhangyuxiaowanzi', 'hebaodan', 'kesong', 'feiyuguantou',
+    'shengjian', 'youtiao', 'yuzijiang', 'zhutongfan', 'doujiang', 'sanmingzhi', 'jiaozi', 'huoguo', 'mantou', 'jiroujuan',]
+
+const dessert = []
+
+const fruit = [
+
+]
+
+const vegetable = [
+
+
+]
 
 export default {
-    food,
+    foods,
 }
