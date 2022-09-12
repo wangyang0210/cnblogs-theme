c108c119ace928bce97af908dec755a9470b5d4d
wangyang <wangyang.0210@foxmail.com>
2022-09-12 19:30:23 +0800

    dist: test

--- dist/script/mouse-mo.5b0ea42c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkcnblogs_theme=self.webpackChunkcnblogs_theme||[]).push([[4170],{50925:function(n,e,t){t.r(e),t.d(e,{default:function(){return c}});var e=t(22707),u=t.n(e);function c(n){new(u().Burst)({radius:{0:100},count:5,children:{shape:"polygon",fill:{cyan:"yellow"},radius:20,rotate:{360:0},duration:2e3}})}}}]);
