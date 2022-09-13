b62f89b01ce90e0e993c14f255ff5208ffb8f2b6
wangyang <wangyang.0210@foxmail.com>
2022-09-13 22:44:31 +0800

    refactor: mojs

--- src/components/mouse/mo.js
+++ src/components/mouse/mo.js
@@ -6,9 +6,11 @@
  * ----------------------------------------------
  * @describe: 使用mo.js实现各种效果
  */
-import mojs from '@mojs/core'
 
 export default function main(options) {
+
+    import(/* webpackChunkName: "mo" */ './lib/mo.umd');
+
     const burst = new mojs.Burst({
         left: 0, top: 0,
         radius:   { 0: 100 },
