bbefa03f926f29d346b5743fb859a4e8414f8191
wangyang <wangyang.0210@foxmail.com>
2022-09-13 22:06:35 +0800

    refactor: mo.js

--- src/components/mouse/mo.js
+++ src/components/mouse/mo.js
@@ -10,17 +10,22 @@ import mojs from '@mojs/core'
 
 export default function main(options) {
     const burst = new mojs.Burst({
-        radius: { 0: 100 },
-        count: 5,
+        left: 0, top: 0,
+        radius:   { 0: 100 },
+        count:    5,
         children: {
-            shape: 'polygon',
-            fill: { 'cyan' : 'yellow' },
-            radius: 20,
-            rotate: { 360: 0 },
-            duration: 2000
+            shape:      'polygon',
+            fill:       { 'cyan' : 'yellow' },
+            radius:     20,
+            rotate:      { 360: 0 },
+            duration:   2000
         }
     });
+
     document.addEventListener( 'click', function (e) {
-        burst.tune({ x: e.pageX, y: e.pageY }).replay();
+        burst
+            .tune({ x: e.pageX, y: e.pageY })
+            .setSpeed(3)
+            .replay();
     });
 }
