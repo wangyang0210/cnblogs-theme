7e9d0951d9fbda119f443ee31db7a46fda7561f9
wangyang <wangyang.0210@foxmail.com>
2022-09-15 16:08:31 +0800

    refactor: code.js

--- src/components/code/code.js
+++ src/components/code/code.js
@@ -28,8 +28,11 @@ export default function main(_) {
             pre.attr('boxid', boxId);
 
             // 增加语言标签
-            let lan = pre.find('code').attr('class').match(/.*(language-[a-zA-Z0-9]+)\s+.*/);
-            if (!!lan && lan.length > 0) pre.addClass(lan[1]);
+            // let codeClass = pre.find('code').attr('class');
+            // if (codeClass) {
+            //     let lan = codeClass.match(/.*(language-[a-zA-Z0-9]+)\s+.*/);
+            //     if (!!lan && lan.length > 0) pre.addClass(lan[1]);
+            // }
         });
     })();
 
