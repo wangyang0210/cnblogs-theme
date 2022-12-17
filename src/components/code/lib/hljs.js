5048ad5a68cc4d7ab554869cfc3ba3d7abf79e38
wangyang <wangyang.0210@foxmail.com>
2022-12-17 10:53:19 +0800

    dist: hljscss

--- src/components/code/lib/hljs.js
+++ src/components/code/lib/hljs.js
@@ -10,72 +10,71 @@ await $.__tools.dynamicLoadingJs($.__config.default.highlight).catch(e => consol
 export default function main(setCodeLine) {
 
     let theme = $.__config.code.options.hljs.theme.toLowerCase()
-    $.__tools.dynamicLoadingCss(`${$.__config.default.hljscss + theme}.min.css`).then(r => {
-        let code  = $('code-box pre code')
-        let bgFlg = $.inArray(theme, [
-            'github-gist', 'googlecode', 'grayscale',
-            'idea', 'isbl-editor-light', 'qtcreator_light',
-            'tomorrow', 'vs', 'xcode', 'arduino-light',
-            'ascetic', 'color-brewer', 'lightfair'
-        ]) !== -1;
+    $.__tools.dynamicLoadingCss(`${$.__config.default.hljscss + theme}.min.css`)
+    let code  = $('code-box pre code')
+    let bgFlg = $.inArray(theme, [
+        'github-gist', 'googlecode', 'grayscale',
+        'idea', 'isbl-editor-light', 'qtcreator_light',
+        'tomorrow', 'vs', 'xcode', 'arduino-light',
+        'ascetic', 'color-brewer', 'lightfair'
+    ]) !== -1;
 
-        /**
-         * 渲染代码
-         */
-        (() => {
-            // 语言范围设置
-            if ( $.__config.code.options.hljs.languages.length) {
-                hljs.configure({
-                    languages: $.__config.code.options.hljs.languages
-                });
-            }
-
-            // 渲染代码
-            $.each(code, function (i, e) {
-                let obj = $(code[i]);
-                // 做一次换行兼容处理/padding处理
-                obj.css('white-space', 'pre').html().replace(/\<br\>/g, '\n');
-                obj.css({'padding': 0})
-                // 清除代码原有样式
-                obj.text(obj.text());
-                // 替换白色背景的主题
-                bgFlg && obj.css('background', '#f5f5fa');
-                // 开始渲染代码
-                hljs.highlightElement(e);
-                // 设置复制按钮颜色
-                $('.clipboard[boxid='+ obj.attr('boxid') +']').addClass('hljs-comment');
-            });
-        })();
-
-        /**
-         * 显示自动识别语言
-         */
-        let setCodeHljsLen = () => {
-            let codeBox = $('code-box');
-            $.each(codeBox, function (i, e) {
-                let obj = $(codeBox[i]);
-                let language = $('pre code')[i].result?.language
-                if (language) obj.find('.code-hljs-len').text(language).css('visibility', 'visible');
+    /**
+     * 渲染代码
+     */
+    (() => {
+        // 语言范围设置
+        if ( $.__config.code.options.hljs.languages.length) {
+            hljs.configure({
+                languages: $.__config.code.options.hljs.languages
             });
         }
 
-        /**
-         * 设置工具条背景色 & 添加语言标签
-         */
-        (() => {
-            $.__timeIds.hljsCodeTId = window.setInterval(() => {
-                let preHljs = $('pre code.hljs');
-                if (preHljs.length > 0) {
-                    $('code-box').css('background', $('pre code.hljs').css('background')).prepend('<hljs-len class="code-hljs-len"></hljs-len>');
-                    setCodeHljsLen();
-                    $.__tools.clearIntervalTimeId( $.__timeIds.hljsCodeTId);
-                }
-            }, 1000);
-        })();
+        // 渲染代码
+        $.each(code, function (i, e) {
+            let obj = $(code[i]);
+            // 做一次换行兼容处理/padding处理
+            obj.css('white-space', 'pre').html().replace(/\<br\>/g, '\n');
+            obj.css({'padding': 0})
+            // 清除代码原有样式
+            obj.text(obj.text());
+            // 替换白色背景的主题
+            bgFlg && obj.css('background', '#f5f5fa');
+            // 开始渲染代码
+            hljs.highlightElement(e);
+            // 设置复制按钮颜色
+            $('.clipboard[boxid='+ obj.attr('boxid') +']').addClass('hljs-comment');
+        });
+    })();
 
-        /**
-         * 设置行号
-         */
-        (() => { setCodeLine(); })();
-    })
+    /**
+     * 显示自动识别语言
+     */
+    let setCodeHljsLen = () => {
+        let codeBox = $('code-box');
+        $.each(codeBox, function (i, e) {
+            let obj = $(codeBox[i]);
+            let language = $('pre code')[i].result?.language
+            if (language) obj.find('.code-hljs-len').text(language).css('visibility', 'visible');
+        });
+    }
+
+    /**
+     * 设置工具条背景色 & 添加语言标签
+     */
+    (() => {
+        $.__timeIds.hljsCodeTId = window.setInterval(() => {
+            let preHljs = $('pre code.hljs');
+            if (preHljs.length > 0) {
+                $('code-box').css('background', $('pre code.hljs').css('background')).prepend('<hljs-len class="code-hljs-len"></hljs-len>');
+                setCodeHljsLen();
+                $.__tools.clearIntervalTimeId( $.__timeIds.hljsCodeTId);
+            }
+        }, 1000);
+    })();
+
+    /**
+     * 设置行号
+     */
+    (() => { setCodeLine(); })();
 }
