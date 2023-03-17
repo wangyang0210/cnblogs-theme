591503111a504cbec28f58b587595eada7a04da2
wangyang.0210@foxmail.com <wangyang.0210@foxmail.com>
2023-03-17 16:11:14 +0800

    refactor: comment del style

--- src/components/comment/comment.js
+++ src/components/comment/comment.js
@@ -8,57 +8,73 @@
  */
 
 export default function main() {
-
     // 评论框打字特效
-    if($.__config.articleContent.commentTyping.enable) {
-        const POWERMODE  = require('./commentTyping/commentTyping')
-        POWERMODE.colorful = $.__config.articleContent.commentTyping.options.colorful;
-        POWERMODE.shake = $.__config.articleContent.commentTyping.options.shake;
-        document.body.addEventListener('input', POWERMODE);
+    if ($.__config.articleContent.commentTyping.enable) {
+        const POWERMODE = require('./commentTyping/commentTyping')
+        POWERMODE.colorful = $.__config.articleContent.commentTyping.options.colorful
+        POWERMODE.shake = $.__config.articleContent.commentTyping.options.shake
+        document.body.addEventListener('input', POWERMODE)
     }
 
     let setComment = () => {
-        let feedbackItem = $('.feedbackItem');
+        let feedbackItem = $('.feedbackItem')
         if (feedbackItem.length > 0) {
-            $.each(feedbackItem, (i) => {
+            $.each(feedbackItem, i => {
                 let obj = $(feedbackItem[i]),
                     feedbackCon = obj.find('.feedbackCon'),
                     feedbackListSubtitle = obj.find('.feedbackListSubtitle'),
                     commentBody = feedbackCon.length ? feedbackCon.find('.blog_comment_body') : [],
                     avatarHtml = '',
-                    idInfo = commentBody.length ? commentBody.attr('id').split('_') : undefined;
+                    idInfo = commentBody.length ? commentBody.attr('id').split('_') : undefined
 
                 if (idInfo && idInfo.length > 0) {
                     let id = idInfo[idInfo.length - 1],
-                        idTmp = id.toString().match(/\d/g);
+                        idTmp = id.toString().match(/\d/g)
 
-                    if ($.isArray(idTmp)) id = idTmp.join('');
+                    if ($.isArray(idTmp)) id = idTmp.join('')
 
-                    let op = $('#comment_' + id + '_avatar'), patch  = op.length > 0 ? op.text().trim() : $.__config.default.avatar;
+                    let op = $('#comment_' + id + '_avatar'),
+                        patch = op.length > 0 ? op.text().trim() : $.__config.default.avatar
 
-                    let ac = $('#a_comment_author_' + id), ah = ac.length ? ac.attr('href') : 'javascropt:void(0);';
+                    let ac = $('#a_comment_author_' + id),
+                        ah = ac.length ? ac.attr('href') : 'javascropt:void(0);'
 
-                    avatarHtml = '<div class="feedbackAvatar"><a href="' + ah + '" target="_blank"><img src="'+patch+'"/></a></div>';
-                    obj.prepend(avatarHtml);
+                    avatarHtml =
+                        '<div class="feedbackAvatar"><a href="' +
+                        ah +
+                        '" target="_blank"><img src="' +
+                        patch +
+                        '"/></a></div>'
+                    obj.prepend(avatarHtml)
                 }
 
-                if (feedbackListSubtitle.length && feedbackListSubtitle.find('.louzhu').length) feedbackListSubtitle.addClass('feedbackListSubtitle-louzhu');
-            });
-            $(feedbackItem[0]).css('padding-top', '0');
-            $(feedbackItem[feedbackItem.length - 1]).css('padding-bottom', '0');
+                if (feedbackListSubtitle.length && feedbackListSubtitle.find('.louzhu').length)
+                    feedbackListSubtitle.addClass('feedbackListSubtitle-louzhu')
+            })
+            $(feedbackItem[0]).css('padding-top', '0')
+            $(feedbackItem[feedbackItem.length - 1]).css('padding-bottom', '0')
             $.__config.animate.avatar.enable && $('.feedbackAvatar').addClass('img-rounded')
         }
     }
 
-    $.__timeIds.commentTId = window.setInterval(() =>{
+    $.__timeIds.commentTId = window.setInterval(() => {
         if ($('.feedbackItem').length > 0) {
-            setComment();
-            $.__tools.clearIntervalTimeId( $.__timeIds.commentTId);
+            setComment()
+            $.__tools.clearIntervalTimeId($.__timeIds.commentTId)
         }
-    },1000);
-
+    }, 1000)
 
     $(document).ajaxSuccess(function (event, xhr, settings) {
-        if (settings.url.includes("GetComments.aspx")) setComment();
-    });
+        if (settings.url.includes('GetComments.aspx')) setComment()
+    })
+
+    $(document).ajaxSuccess(function (event, xhr, settings) {
+        if (settings.url.includes('DeleteComment.aspx')) {
+            let obj = $('.feedbackItem .feedbackCon .blog_comment_body')
+            $.each(obj, i => {
+                console.log(!$(obj[i]).text())
+                if (!$(obj[i]).text()) console.log($(obj[i]).parent().parent().remove())
+            })
+        }
+    })
 }
